75abae67193c496790ef56f283363a002795105a
MrSia10 <siavashshahkar@yahoo.com>
2022-04-09 19:16:06 -0400

    *

--- 1D Convection/1D Convection.py
+++ 1D Convection/1D Convection.py
@@ -10,7 +10,7 @@ def step(t, t_end, step_value):
 
 
 #Define the function to calculate the first derivative
-def d1_conv (dx, q, N):
+def d1_conv(dx, q, N):
     N = q.shape[0]
     dqdx = np.zeros_like(q)
     dqdx[0] = (q[1] - q[0]) / dx
@@ -19,6 +19,17 @@ def d1_conv (dx, q, N):
         dqdx[i] = (q[i] - q[i-1]) / dx
     return dqdx
 
+def d1_diff(dx, q, N):
+    N = q.shape[0]
+    dqdx2 = np.zeros_like(q)
+    dqdx2[0] = (q[2] + q[0] - 2 * q[1]) / (dx ** 2)
+
+    for i in np.arange(1, N-1):
+        dqdx2[i] = (q[i+1] - 2 * q[i] + q[i-1]) / (dx ** 2)
+
+    return dqdx2
+
+
 #time variables
 t = 0
 t_final = 0.75
@@ -26,7 +37,7 @@ dt = 1e-4
 
 #physical parameters
 L = 1
-c = 1
+b = 0.07
 
 #space parameters
 N = 200
@@ -36,6 +47,7 @@ dx = L / N
 q1 = np.zeros(N + 1)
 q1_old = np.zeros(N + 1)
 dq1dx = np.zeros(N + 1)
+dq2dx = np.zeros(N + 1)
 
 x = np.zeros(N + 1)
 
@@ -48,13 +60,16 @@ while t < t_final:
     q1[0] = step(t, 0.2, 1)
 
     dq1dx = d1_conv(dx, q1_old, N)
+    dq2dx = d1_diff(dx, q1_old, N)
 
-    q1[1: -1] = q1_old[1: -1] - c * dt * dq1dx[1: -1]
+    q1[1: -2] = q1_old[1: -2] + b * dt * dq2dx[1: -2] - q1_old[1: -2] * dt * dq1dx[1: -2]
+    q1[-1] = q1[-2]     #zero gradient boundary layer
     q1_old = q1
 
-plt.plot(x, q1, label='1D convection')
+plt.plot(x, q1, label= "1D Burger's")
 plt.legend()
 plt.grid()
 plt.show()
+
 
 
