e169ae1d6a31d7554ab263c287bb4d0f92e4d0a2
MrSia10 <siavashshahkar@yahoo.com>
2022-04-10 20:51:45 -0400

    new file

--- 2D Burgers/2D Burgers.py
+++ 2D Burgers/2D Burgers.py
@@ -16,22 +16,24 @@ def step(t, t_end, step_value):
 #Define the function to calculate the first derivative
 def conv(dx, u, N):
     N = u.shape[0]
-    dudx1 = np.zeros_like((u, 2))
+    dudx1 = np.zeros((len(u),2))
     dudx1[0, 0] = (u[1, 0] - u[0, 0]) / dx
+    dudx1[N, 0] = 0
 
     for j in np.arange(0, 1):
-        for i in np.arange(1, N - 1):
+        for i in np.arange(1, N):
             dudx1[i, j] = (u[i, j] - u[i-1, j]) / dx
     return dudx1
 
 
 def diff(dx, u, N):
     N = u.shape[0]
-    dudx2 = np.zeros_like((u, 2))
+    dudx2 = np.zeros((len(u), 2))
     dudx2[0, 0] = (u[2, 0] + u[0, 0] - 2 * u[1, 0]) / (dx ** 2)
+    dudx2[N, 0] = 0
 
     for j in np.arange(0, 1):
-        for i in np.arange(1, N-1):
+        for i in np.arange(1, N):
             dudx2[i, j] = (u[i+1, j] - 2 * u[i, j] + u[i-1, j]) / (dx ** 2)
 
     return dudx2
@@ -53,10 +55,10 @@ dx = L / N
 dy = L / M
 
 #Initialize solution arrays
-u1 = np.zeros((N + 1, M + 1))
-u1_old = np.zeros((N + 1, M + 1))
-v1 = np.zeros((N + 1, M + 1))
-v1_old = np.zeros((N + 1, M + 1))
+u1 = np.zeros((N + 1, 2))
+u1_old = np.zeros((N + 1, 2))
+v1 = np.zeros((N + 1, 2))
+v1_old = np.zeros((N + 1, 2))
 
 x = np.zeros(N + 1)
 y = np.zeros(M + 1)
