cbf6f2f2f7f175d668b4d552154393511e3bc344
BigRed001 <cole.kennedy1669@gmail.com>
2022-04-13 15:33:23 -0400

    Poisson

--- 2D Poisson.py
+++ 2D Poisson.py
@@ -5,23 +5,35 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# Define Parameters
+def plot2D(x, y, p):
+    fig = plt.figure(figsize=(11, 7), dpi=100)
+    ax = fig.add_subplot(111, projection='3d')
+    X, Y = np.meshgrid(x, y)
+    surf = ax.plot_surface(X, Y, p[:], rstride=1, cstride=1, cmap=cm.viridis,
+            linewidth=0, antialiased=False)
+    ax.view_init(30, 225)
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    plt.show()
 
 # physical parameters
-L = 1  # If both the length of the x and y are the same
+
+lx = 1
+ly = 1
+
 # space parameters
 N = 200
 M = 200
 nt = 100 # number of timesteps
-dx = L / ( N -1)
-dy = L / ( M -1)
+dx = lx / (N - 1)
+dy = ly / (M - 1)
 
 # Initialization
 p = np.zeros((M, N))
 pd = np.zeros((M, N))
 b = np.zeros((M, N))
-x = np.linspace(L, N)
-y = np.linspace(L, M)
+x = np.linspace(0, lx, N)
+y = np.linspace(0, ly, M)
 
 # Source to be added to laplace equation
 b[int(M / 4), int(N / 4)] = 100
@@ -31,29 +43,16 @@ for it in range(nt):
 
     pd = p.copy()
 
-    p[1:-1 ,1:-1] = (((pd[1:-1, 2:] + pd[1:-1, :-2]) * dy**2 +
-                    (pd[2:, 1:-1] + pd[:-2, 1:-1]) * dx**2 -
-                    b[1:-1, 1:-1] * dx**2 * dy**2) /
-                    (2 * (dx**2 + dy**2)))
+    p[1:-1, 1:-1] = (((pd[1:-1, 2:] + pd[1:-1, :-2]) * dy ** 2 +
+                      (pd[2:, 1:-1] + pd[:-2, 1:-1]) * dx ** 2 -
+                      b[1:-1, 1:-1] * dx ** 2 * dy ** 2) /
+                     (2 * (dx ** 2 + dy ** 2)))
 
     p[0, :] = 0
-    p[ M -1, :] = 0
+    p[M -1, :] = 0
     p[:, 0] = 0
-    p[:, N- 1] = 0
+    p[:, N - 1] = 0
 
-
-def plot2D(x, y, p):
-    fig = plt.figure(figsize=(11, 7), dpi=100)
-    ax = fig.add_subplot(111, projection='3d')
-    X, Y = np.meshgrid(x, y)
-    surf = ax.plot_surface(X, Y, p[:], rstride=1, cstride=1, cmap=cm.viridis,
-                           linewidth=0, antialiased=False)
-    ax.set_xlim(0, 2)
-    ax.set_ylim(0, 1)
-    ax.view_init(30, 225)
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$y$')
-    plt.show()
 
 
 plot2D(x, y, p)
