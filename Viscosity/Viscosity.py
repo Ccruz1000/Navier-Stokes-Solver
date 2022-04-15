adb42a208837ae46c65c7ee94170681b0bfdc8eb
MrSia10 <siavashshahkar@yahoo.com>
2022-04-15 12:14:04 -0400

    Viscosity

--- Viscosity/Viscosity.py
+++ Viscosity/Viscosity.py
@@ -1,10 +1,11 @@
 import numpy as py
 from function1d import fir_o_back_y2d
+from function1d import fir_o_back_x2d
 
-dudx = fir_o_back_y2d(dx, u, N)
+dudx = fir_o_back_x2d(dx, u, N)
 dvdy = fir_o_back_y2d(dy, v, N)
 dudy = fir_o_back_y2d(dy, u, N)
-dvdx = fir_o_back_y2d(dx, v, N)
+dvdx = fir_o_back_x2d(dx, v, N)
 
 miu = 1
 lamb = -2 / 3 * miu
