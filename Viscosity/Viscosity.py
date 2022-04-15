6abf24e44d23fc59b17231b47472aed163c636eb
MrSia10 <siavashshahkar@yahoo.com>
2022-04-15 12:08:44 -0400

    Viscosity

---
+++ Viscosity/Viscosity.py
@@ -0,0 +1,19 @@
+import numpy as py
+from function1d import fir_o_back_y2d
+
+dudx = fir_o_back_y2d(dx, u, N)
+dvdy = fir_o_back_y2d(dy, v, N)
+dudy = fir_o_back_y2d(dy, u, N)
+dvdx = fir_o_back_y2d(dx, v, N)
+
+miu = 1
+lamb = -2 / 3 * miu
+
+def tau(dudx, dvdy, miu, lamb):
+    tau = (lamb + 2 * miu) * dudx + lamb * dvdy
+    return tau
+
+def tauxy(dudy, dvdx, miu):
+    tau = miu * (dudy + dvdx)
+    return tau
+
