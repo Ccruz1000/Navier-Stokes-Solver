dee08153406004c403d0b17951290a3139ae54f2
Rayan <rayan_kaniah"hotmail.com>
2022-04-17 18:59:12 -0400

    post stress and heat flux

--- Test/MainFlatPlate.py
+++ Test/MainFlatPlate.py
@@ -140,10 +140,27 @@ def calculateF(primitives, dx, dy, direction):
 def calculateU(primitives):
     U = np.zeros(np.shape(primitives.u,1),np.shape(primitives.u,2),4);
     U[:,:,0] = primitives.r
-    U[:,:,1] = U[:,:,1]*primitives.u
-    U[:,:,2] = U[:,:,1]*primitives.v
+    U[:,:,1] = U[:,:,0]*primitives.u
+    U[:,:,2] = U[:,:,0]*primitives.v
     U[:,:,3] = primitives.Et
     return U
+
+def postStressAndHeatFlux(primitives,x,y):
+    dx = x[0,1]-x[0,0]
+    dy = y[1,0]-y[0,0]
+    [u,v,[],T] = primitives.deal()
+    [mu,lam,k] = primitives.getMuLambdaK()
+    dudx = ddxc(u,dx)
+    dudy = ddyc(u,dy)
+    dvdx = ddxc(v,dx)
+    dvdy = ddyc(v,dy)
+    txx = lam*(dudx + dvdy)+ 2*mu*(dudx)
+    tyy = lam*(dudx + dvdy)+ 2*mu*(dvdy)
+    txy = mu*(dudy + dvdx)
+    dTdx = ddxc(T,dx)
+    dTdy = ddyc(T,dy)
+    qx = -k*dTdx
+    qy = -k*dTdy
 # Plate length
 lhori = 0.00001  # m
 
