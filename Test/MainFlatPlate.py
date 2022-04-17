c82a0b4c44200e53de23c678ed1bafd8e728f415
BigRed001 <cole.kennedy1669@gmail.com>
2022-04-17 17:42:05 -0400

    Added Main Parameters

--- Test/MainFlatPlate.py
+++ Test/MainFlatPlate.py
@@ -2,22 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+# Plate length
+lhori = 0.00001  # m
 
-# Define Domain
-num_x = 20  # Number of X points
-num_y = 20  # Number of Y points
-x_end = 0.3048  # Final Point in x
-x_start = 0.05  # First point in x (absolute value, shifted to left on x axis)
-y_end = 0.05  # Final Point in y
-y_start = 0
-MAXIT = 1e4  # Number of time steps allowed before stopping program
+# Courant number
+K = 0.8
 
-# Inflow Conditions
+# grid size and max iterations
+nx = 70
+ny = 70
+maxiter = 10000
 
-M_inf = 4  # Free stream mach number
-T_inf = 288.16  # Free stream temperature (K)
-p_inf = 101325  # Free stream pressure (pa)
-inflow = Primitives(0, 0, p_inf,T_inf)
-inflow.u = M_inf*inflow.a # m/s
-Re_inf = inflow.calculateReynoldsNumber(x_end)
+# Inflow conditions
+Minf = 4
+pinf = 101325  # Pa
+Tinf = 288.16  # Kelvin
+inflow = Primitives(0, 0, pinf, Tinf)
+inflow.u = Minf * inflow.a  # m/s
+Reinf = inflow.calculateReynoldsNumber(lhori)
 
+# boundary layer size & vertical domain size
+delta = 5 * lhori / np.sqrt(Reinf)
+lvert = 5 * delta
+
+# grid
+x, y = np.meshgrid(np.linspace(0, lhori, nx), np.linspace(0, lvert, ny))
+
+# Set initial conditions
+# Set all intial values to inflow values. Conditions on boundaries updated by solveMacCormack()
+primitives = Primitives(inflow.u * np.ones(ny, nx), inflow.v * np.ones(ny, nx), inflow.p * np.ones(ny, nx), \
+                        inflow.T * np.ones(ny, nx))
+
+# Solve two wall temperature conditions
+Tw_Tinf = 1.0  # constant wall temperature
+constantTw = solveMacCormack(primitives, inflow, Tw_Tinf, K, x, y, maxiter)
